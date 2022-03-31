7c926fcf07a76adbd54722cc3f1fdf6d2888d766
NAWEL <naowel@hotmail.fr>
2022-03-31 10:23:48 +0200

    ajout url bdd

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express=require("express")
 const app= express()
+const dbConfig = require('./db')
+
+
 const PORT= process.env.PORT ||5000;
+
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
