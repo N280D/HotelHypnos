2df63a5fa1f2ca919df797b0692fff2ef837d6ad
NAWEL <naowel@hotmail.fr>
2022-04-01 15:08:23 +0200

    update emplacement fichier back end

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express=require("express")
 const app= express()
 const dbConfig = require('./db')
+const roomsRoute=require('./routes/roomRoute')
+
+app.use('/api/rooms',roomsRoute)
 
 
 const PORT= process.env.PORT ||5000;
