f9bd6ad1bcccf5c4cffe5dd7c946fbceccd20105
N280D <NAOWEL@HOTMAIL.FR>
2022-04-04 12:14:17 +0200

    update vers mac

--- models/room.js
+++ models/room.js
@@ -25,6 +25,6 @@ const roomSchema= mongoose.Schema({
     timestamps:true,
 })
 
-const roomModel= mongoose.model('room',roomSchema)
+const roomModel= mongoose.model('rooms',roomSchema)
 
 module.exports=roomModel
